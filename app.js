16ccdf43b2a2f3a79cb9864e53d7dcf3cb66b080
Kam Cheng <kam.cheng03@gmail.com>
2024-04-15 14:17:13 +0100

    add get api endpoint for object detailing all endpoints in api 3-endpoints complete

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const {getTopics} = require('./controllers/topics.controller');
+const endpoints = require('./endpoints.json');
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", (req, res, next) => {
+    res.status(200).send(endpoints)
+})
 
 app.all("*", (req, res, next) => {
     res.status(404).send({message: "path not found"});
